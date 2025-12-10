a79e944740abbe60c0c94fc057ae08cc961e5f07
buith <buithaibao2k4@gmail.com>
2025-12-10 23:48:07 +0700

    update

--- src/pages/home/HomePageNew.jsx
+++ src/pages/home/HomePageNew.jsx
@@ -3,16 +3,11 @@ import { Link, useNavigate } from "react-router-dom";
 
 import HeroSection from "../../components/home/HeroSection.jsx";
 
-const products = {
-  name: "Welcome to Pet Shop",
-  age: "20",
-};
-
 const HomePageNew = () => {
   const navigate = useNavigate();
   return (
     <section>
-      <HeroSection products={products} />
+      <HeroSection />
     </section>
   );
 };
